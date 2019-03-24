05fc460411494878721ae4ce45f5fb7e90afad59
cameron-chromiak <cam.chromiak@hotmail.com>
2019-03-24 15:33:36 -0400

    more practice questions!

--- inde.js
+++ inde.js
@@ -86,18 +86,53 @@
 //   console.log(arr);
 // }
 // sortThis()
+//
+// const formatDuration = ms => {
+//   if (ms < 0) ms = -ms;
+//   const time = {
+//     day: Math.floor(ms / 86400000),
+//     hour: Math.floor(ms / 3600000) % 24,
+//     minute: Math.floor(ms / 60000) % 60,
+//     second: Math.floor(ms / 1000) % 60,
+//     millisecond: Math.floor(ms) % 1000
+//   };
+//   return Object.entries(time)
+//     .filter(val => val[1] !== 0)
+//     .map(([key, val]) => `${val} ${key}${val !== 1 ? 's' : ''}`)
+//     .join(', ');
+// };
 
-const formatDuration = ms => {
-  if (ms < 0) ms = -ms;
-  const time = {
-    day: Math.floor(ms / 86400000),
-    hour: Math.floor(ms / 3600000) % 24,
-    minute: Math.floor(ms / 60000) % 60,
-    second: Math.floor(ms / 1000) % 60,
-    millisecond: Math.floor(ms) % 1000
-  };
-  return Object.entries(time)
-    .filter(val => val[1] !== 0)
-    .map(([key, val]) => `${val} ${key}${val !== 1 ? 's' : ''}`)
-    .join(', ');
-};
+//fibb
+// let n1 = 1
+// let n2 = 1
+//
+// while(n1 < 25){
+//   n1 = n1 + n2
+//   console.log('n2',n2);
+//   n2 = n1 - n2
+//
+//   console.log(n1);
+// }
+//
+// //====
+// var x = '6'
+// var y = 6
+//
+// console.log(x === y);
+// console.log(x == y);
+
+// let vs var
+
+// helpMe = () =>{
+//   for(let i = 0; i<10; i++){
+//     console.log(i);
+//   }
+//   // console.log(i);
+//
+//   for(var j = 0; j<10; j++){
+//     console.log('j',j);
+//   }
+//   console.log('outside j',j);
+// }
+//
+// helpMe()
