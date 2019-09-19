7718234aa94413ca3e0ebd8dd2f39fa4627a519a
cameron-chromiak <cam.chromiak@hotmail.com>
2019-09-19 16:30:04 -0400

    -helped w someone's rover

--- inde.js
+++ inde.js
@@ -64,6 +64,8 @@
 // }
 // fibbonocci()
 
+
+//change var to let
 // function scope(){
 //   for(var i = 0; i<5; i++){
 //     console.log(i);
@@ -225,13 +227,205 @@
 //   console.log(reword);
 // })()
 
+//
+// function isPalindrome(){
+//   let word = 'racecar'
+//
+//   let arr = word.split('').reverse().join('')
+//   console.log((arr == word) ? true:false);
+//
+// }
+//
+// isPalindrome()
 
-function isPalindrome(){
-  let word = 'racecar'
 
-  let arr = word.split('').reverse().join('')
-  console.log((arr == word) ? true:false);
+// Write a function that receives a radius as a parameter, and returns the area of a circle: Pi * r^2
+//
+// function calcArea(r){
+//     return (r**2) * 3.14
+// }
+//
+// calcArea(2)
 
+// Write a function that takes in a time as a string and returns it in Military Time.
+// “10:31 AM” -> “10:31”
+// “2:05 PM” -> “14:05”
+// “12:01 AM” -> “00:01”
+// “7:00 AM” -> “07:00”
+//
+// function convertTime(time){
+//
+//   if(time.includes('PM')){
+//     let temp = time.split(' ')
+//     temp.pop()
+//     hoursAndMinutes = temp[0].split(':')
+//     let hour = Number(hoursAndMinutes[0]) + 12
+//     let min = hoursAndMinutes[1]
+//     console.log(`${time} converted is ${hour}:${min}`)
+//   }
+//   else if(time.includes('AM')){
+//     let temp = time.split(' ')
+//     temp.pop()
+//     let hoursAndMinutes = temp[0].split(':')
+//     let hour = hoursAndMinutes[0]
+//     let min = hoursAndMinutes[1]
+//     if(hour.length <= 1){
+//       hour =  '0' + hoursAndMinutes[0]
+//     }
+//     (hour == '12') ?  hour = '00' : hour
+//     console.log(`${time} converted is ${hour}:${min}`)
+//   }
+//   else{
+//     console.log(`Improper format, ${time}`);
+//   }
+//
+// }
+//
+//
+// times = ['10:31 AM', '2:05 PM', '12:01 AM', '7:00 AM', '12:45 AM', '12: 34', '7:47 PM', '2:04 AM', '5:55 PM']
+//
+// times.forEach( time =>{
+//   convertTime(time)
+// })
+//
+// determine if it is AM or PM
+// remove 'AM' or 'PM' splitting at space
+// split again at colon
+// if (AM)
+//   hour will be first element in that array, do math on it
+//   concat new hour with minutes
+// if (PM):
+//   determine if hour value need a 0 to make the end length 4 instead of 3 (3:00 --> 03:00)
+//   otherwise hour = hours
+//   print and concat
+
+
+
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+
+
+// //Cameron Chromiak
+// //cameron.chromiak@gmail.com
+// //Wayfair coding challenge 7-12-19
+// function solution(S) {
+//   let fileSize = {
+//     music: 0,
+//     images: 0,
+//     movies: 0,
+//     other: 0
+//   }
+//
+//   let files = S.split('\n')
+//
+//   files.forEach(file =>{
+//     if (file.includes('.mp3') || file.includes('.flac')){
+//       let size = file.split(' ')[1]
+//       fileSize.music += parseInt(size.split('b')[0])
+//     }
+//     else if (file.includes('.mkv') || file.includes('.mp4')){
+//       let size = file.split(' ')[1]
+//       fileSize.movies += parseInt(size.split('b')[0])
+//     }
+//     else if (file.includes('.img') || file.includes('.jpg')){
+//       let size = file.split(' ')[1]
+//       fileSize.images += parseInt(size.split('b')[0])
+//     }
+//     else{
+//       let size = file.split(' ')[1]
+//       fileSize.other += parseInt(size.split('b')[0])
+//     }
+//   })
+//   return formatReturn(fileSize)
+//   // console.log(fileSize);
+// }
+//
+// function formatReturn(files){
+//   Object.keys(files).forEach(function(key) {
+//     console.log(key, files[key] + 'b');
+//   });
+// }
+
+// solution( 'my.song.mp3 11b\ngreatSong.flac 1000b\nnot3.txt 5b\nvideo.mp4 200b\ngame.exe 100b\nmov!e.mkv 10000b' )
+
+
+const rover = {
+  direction: "N",
+  x: 0,
+  y: 0
 }
 
-isPalindrome()
+function turnLeft(rover) {
+  console.log("turnLeft() was called!")
+  switch (rover.direction) {
+    case "N":
+      rover.direction = "W";
+      break;
+    case "W":
+      rover.direction = "S";
+      break;
+    case "S":
+      rover.direction = "E";
+      break;
+    case "E":
+      rover.direction = "N";
+      break;
+  }
+};
+
+function turnRight(rover) {
+  console.log('turnRight() was called');
+  switch (rover.direction) {
+    case "N":
+      rover.direction = "E";
+      break;
+    case "E":
+      rover.direction = "S";
+      break;
+    case "S":
+      rover.direction = "W";
+      break;
+    case "W":
+      rover.direction = "N";
+      break;
+      console.log("turnRight was called!")
+  }
+}
+
+  function moveForward(rover) {
+    console.log('moveForward() was called');
+    switch (rover.direction) {
+      case "N":
+        rover.y += 1;
+        break;
+      case "S":
+        rover.y -= 1;
+        break;
+      case "E":
+        rover.x += 1;
+        break;
+      case "W":
+        rover.x-= 1;
+    };
+    console.log(rover.position)
+  };
+
+  function roverCommand(command) {
+    for (i = 0; i < command.length; i++) {
+      switch (command[i].toUpperCase()) {
+        case "L":
+          turnLeft(rover);
+          break;
+        case "R":
+          turnRight(rover);
+          break;
+        case "F":
+          moveForward(rover);
+          break;
+      }
+    }
+    console.log(rover);
+  };
+
+  roverCommand("FLF")
